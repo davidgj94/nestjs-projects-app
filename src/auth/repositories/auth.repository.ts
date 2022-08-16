9936b6d65cf637504674b9df8fe43e1c29099a78
David Gallardo <davidgj1994@gmail.com>
2022-08-16 17:49:52 +0200

    delete repos

--- src/auth/repositories/auth.repository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-import { AuthenticationEntity } from '../entities/auth.entity';
-
-@EntityRepository(AuthenticationEntity)
-export class AuthenticationRepository extends Repository<AuthenticationEntity> {}
